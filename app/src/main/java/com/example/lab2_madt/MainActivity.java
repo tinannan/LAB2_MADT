7c58769b3c310251263e451af6efd5ec8b0acb9d
tinannan <yashenkoalevtina@gmail.com>
2024-09-23 17:29:49 +0300

    spinner element and basic design

--- app/src/main/java/com/example/lab2_madt/MainActivity.java
+++ app/src/main/java/com/example/lab2_madt/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab2_madt;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Spinner spinnerCounting = (Spinner) findViewById(R.id.spinnerCounting);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.counting_options,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCounting.setAdapter(adapter);
     }
 }
